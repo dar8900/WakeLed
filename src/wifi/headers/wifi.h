55381160b60b15b04f1ab3230fb9545d257e97c3
Dario VNITRO <dar8900@gmail.com>
2021-01-22 23:29:06 +0100

    -Inserite le funzioni per leggere la data on line e iniziata implementazione per raccogliere le informazioni sul meteo

--- src/wifi/headers/wifi.h
+++ src/wifi/headers/wifi.h
@@ -4,7 +4,48 @@
 #include <NTPClient.h>
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
 #include <WiFiUdp.h>
+#include <string>
+#include <Chrono.h>
 
+typedef std::string WifiString;
+
+class WIFI_STATION
+{
+    private:
+        const char *SSID = "FASTWEB-85B1EA";
+        const char *PASSWD = "ZGFyaW9zYXJhYmFzZTY0";
+        WiFiUDP *ntpUDP;
+        NTPClient *timeClient; //(ntpUDP, "europe.pool.ntp.org", 3600, 60000);
+        const WifiString NTP_SERVER = "europe.pool.ntp.org";
+        const WifiString BOLOGNA_ID = "3181927";
+        const WifiString WEATHER_API_KEY = "ec79bf368720417f78abda66d4e47828";
+        WifiString apiWeatherCall;
+        uint16_t localHourShift = 3600; // shift di 1h rispetto all'ora di greenwitch
+        uint16_t timeRefreshFrq = 1000; // frequenza di rischiesta di orario (in ms)
+        bool wifiConnected = false;
+        HTTPClient *httpWeatherReq;
+        Chrono *weatherTimer;
+        void connectToWifi();
+        void getWeatherParameters();
+
+    public:
+        typedef struct 
+        {
+            uint16_t weatherID; // da controllare a pag https://openweathermap.org/weather-conditions
+            float temperature; // in Kelvin, togliere 273.15
+            float perceivedTemp; // in Kelvin, togliere 273.15
+            int32_t humidity; // percentuale
+            float pressure; // in mbar, dividere per 1000
+        }WEATHER_VARS;
+        
+        WIFI_STATION();
+        void initWifiStation();
+        uint32_t getTimestamp();
+        WifiString getTimeFormatted();
+        void run();
+};
 
 #endif
+
--- src/wifi/sources/wifi.cpp
+++ src/wifi/sources/wifi.cpp
@@ -1,1 +1,73 @@
 #include "../headers/wifi.h"
+
+WIFI_STATION::WIFI_STATION()
+{
+    ntpUDP = new WiFiUDP();
+    timeClient = new NTPClient(*ntpUDP, NTP_SERVER.c_str(), localHourShift, timeRefreshFrq);
+    httpWeatherReq = new HTTPClient();
+    weatherTimer = new Chrono(Chrono::SECONDS);
+}
+
+void WIFI_STATION::connectToWifi()
+{
+    uint16_t Timeout = 100;
+    bool WifiConnected = false;
+    WiFi.begin(SSID, PASSWD);
+    while(!WifiConnected && Timeout > 0)
+    {
+        if(WiFi.status() != WL_CONNECTED)
+        {
+            Timeout--;
+        }
+        else
+        {
+            WifiConnected = true;
+        }
+        delay(100);
+    }
+    wifiConnected = WifiConnected;
+}
+
+
+void WIFI_STATION:: getWeatherParameters()
+{
+    if(weatherTimer->hasPassed(5, true))
+    {
+        httpWeatherReq->begin(apiWeatherCall.c_str());
+        int httpResponseCode = httpWeatherReq->GET();
+        if(httpResponseCode > 0)
+        {
+            WifiString Payload = WifiString(httpWeatherReq->getString().c_str());
+            DynamicJsonDocument weatherInfoJASON(1024);
+            deserializeJson(weatherInfoJASON, Payload.c_str());
+        }
+    }
+}
+
+
+
+void WIFI_STATION::initWifiStation()
+{
+    apiWeatherCall = "api.openweathermap.org/data/2.5/weather?id=" + BOLOGNA_ID + "&appid=" + 
+                        WEATHER_API_KEY;
+    connectToWifi();
+    if(wifiConnected)
+    {
+        timeClient->begin();
+    }
+}
+
+uint32_t WIFI_STATION::getTimestamp()
+{
+    return timeClient->getEpochTime();
+}
+
+WifiString WIFI_STATION::getTimeFormatted()
+{
+    return WifiString(timeClient->getFormattedTime().c_str());
+}
+
+void WIFI_STATION::run()
+{
+    
+}
